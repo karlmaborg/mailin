a91d8e3e84d32592e768effb98d306be32a35256
gusnyq <gustav.nykvist@gmail.com>
2015-12-31 00:10:12 +0100

    basic auth

--- cli.js
+++ cli.js
@@ -26,6 +26,9 @@ program.version(pkg.version)
     .option('-p, --port <n>', 'The port to which the mailin smtp server should listen to. Default to 25.', parseInt)
     .option('-w, --webhook [url]', 'The webhook url to which the parsed emails are posted. Default to http://localhost:3000/webhook.')
     .option('-l, --log-file [file path]', "The log file path. Default to '/var/log/mailin.log'.")
+    .option('-h, --host [host]', "ip-addr to listen on")
+    .option('-u, --user [username]', "username for basic auth")
+    .option('-pw, --password [password]', "password for basic auth")
     .option('--disable-dkim', 'Disable dkim checking. The dkim field in the webhook payload will be set to false.')
     .option('--disable-spf', 'Disable spf checking. The spf field in the webhook payload will be set to false.')
     .option('--disable-spam-score', 'Disable spam score computation. The spamScore field in the webhook payload will be set to 0.0.')
@@ -50,6 +53,9 @@ mailin.start({
     port: program.port || 25,
     webhook: program.webhook || 'http://localhost:3000/webhook',
     logFile: program.logFile || '/var/log/mailin.log',
+    host: program.host || '127.0.0.1',
+    user: program.user || 'user',
+    pass: program.password || 'pass',
     disableDkim: program.disableDkim,
     disableSpf: program.disableSpf,
     disableSpamScore: program.disableSpamScore,
